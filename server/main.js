0d17d7657156fb5c11dc2ea10c0db048251af54a
LuisMeraz01227 <luis_mbs05@hotmail.com>
2023-12-04 09:20:44 -0700

    Creacion carpeta public con respectivos archivos, y prueba de frontend en index.html

--- server/main.js
+++ server/main.js
@@ -5,12 +5,19 @@ var app = express();
 /* Aqui estara toda la funcionalidad de los socket, es recomendable usar el moudlo http
    para pasarle la app a express y manejar bien http */
 var server = require('http').Server(app);
-
-
 var io = require('socket.io')(server);
+
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
     res.status(200).send("Hola mundo");
+});
+
+/* De esta forma activamos socket para que este escuchando mandamos un mensaje de control por consola para saber que pasa y tenemos que hacer que el mensaje venga del navegador web
+   mediante html y JS */
+io.on('connection' , function (socket){
+    console.log('Alguien se ha conectado con socket');
 });
 
 server.listen(3005, function(){
